2152fe5bbe149fef5ce47f47bad79224f2f266b0
Sourav Majumdar <souravmajumdar.developer@gmail.com>
2020-11-18 10:15:15 +0530

    Adding a conditional rendaring

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, errMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errMessage: err.message });
+      }
+    );
   }
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.log(error)
-    );
-    return <div>Latitude: </div>;
+    if (this.state.errMessage && !this.state.lat) {
+      return <div>Error: {this.state.errMessage}</div>;
+    } else if (this.state.lat && !this.state.errMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    } else {
+      return <div>Loading...</div>;
+    }
   }
 }
 ReactDOM.render(<App />, document.querySelector('#root'));
